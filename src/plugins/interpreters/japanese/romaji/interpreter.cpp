qt-users-jp/qimsys

commit 7ef895545f5c7bc5b45d40756019ffdfaff8b832

Fix a crash when input skaa or kfui
Change-Id: I1f557a2ce3621909ca9491ece7a5bf9576bed311
Reviewed-on: http://cr.qtquick.me/893
Tested-by: http://ci.qtquick.me/
Reviewed-by: Takahiro Hashimoto <kenya888@qtquick.me>
Tested-by: Takahiro Hashimoto <kenya888@qtquick.me>
Tested-by: Tasuku Suzuki <stasuku@gmail.com>
Reviewed-by: Tasuku Suzuki <stasuku@gmail.com>

src/plugins/interpreters/japanese/romaji/interpreter.cpp
@@ -237,7 +237,7 @@ void Interpreter::Private::itemChanged(const QimsysPreeditItem &item)
             }
             break;
         } else {
-            if (to.at(i) == from.at(i) && from.at(i) == rawString.at(i)) {
+            if (to.at(i) == from.at(i)) {
                 t += to.at(i);
                 f += from.at(i);
                 rS += rawString.at(i);

qimsystest.pri
@@ -4,7 +4,8 @@ QIMSYSTEST_PRI = 1
 include(./qimsys.pri)
 contains(QIMSYS_CONFIG, no-gui): QT -= gui
 TEMPLATE = app
-QT += testlib
+QT += testlib concurrent
+
 CONFIG += testcase
 DESTDIR = $$QIMSYS_BUILD_TREE/$$QIMSYS_TARGET_PATH/$$QIMSYS_APP_PATH
 QMAKE_RPATHDIR += \$\$ORIGIN/../$$QIMSYS_LIBRARY_PATH

src/apps/qimsys/application.cpp
@@ -161,7 +161,7 @@ void Application::Private::setPluginPath()
 
     qimsysDebug() << rootDir.absolutePath();
     setQimsysPluginPath(rootDir.absolutePath());
-#ifndef Q_WS_X11
+#ifndef Q_OS_LINUX
     if (rootDir.exists(QLatin1String("inputmethods"))) {
         q->addLibraryPath(rootDir.absolutePath());
     }

src/libs/qt/items/qimsysconversionitem.h
@@ -32,7 +32,7 @@ struct QIMSYS_EXPORT QimsysConversionItem {
     QString hint;
     QString source;
 
-    inline bool operator==(const QimsysConversionItem &other) {
+    inline bool operator==(const QimsysConversionItem &other) const {
         return (from == other.from && to == other.to && hint == other.hint && source == other.source);
     }
 };

src/libs/qt/qt.pri
@@ -10,6 +10,8 @@ INCLUDEPATH += $$PWD/plugins
 DEPENDPATH += $$PWD/plugins
 INCLUDEPATH += $$PWD/server
 DEPENDPATH += $$PWD/server
+INCLUDEPATH += $$PWD/test
+DEPENDPATH += $$PWD/test
 INCLUDEPATH += $$PWD/utils
 DEPENDPATH += $$PWD/utils
 INCLUDEPATH += $$PWD/widgets

src/libs/qt/test/qimsystest.cpp
@@ -84,7 +84,7 @@ void QimsysTest::initTestCase()
 
     qimsysDebug() << rootDir.absolutePath();
     setQimsysPluginPath(rootDir.absolutePath());
-#ifndef Q_WS_X11
+#ifndef Q_OS_LINUX
     if (rootDir.exists(QLatin1String("inputmethods"))) {
         q->addLibraryPath(rootDir.absolutePath());
     }

tests/system/inputmethods/japanese/standard/main.cpp
@@ -20,6 +20,7 @@
 
 #include <QtTest>
 #include <QtGui>
+#include <QtWidgets>
 
 #include <qimsysdebug.h>
 #include <qimsysapplicationmanager.h>
@@ -90,34 +91,6 @@ private:
 };
 
 void JapaneseStandardTest::initTestCase() {
-    bool found = false;
-    QDir dir(QCoreApplication::applicationDirPath());
-    while (!found && dir.cdUp()) {
-        foreach(const QString &subdir, dir.entryList(QDir::Dirs)) {
-            if (subdir == QLatin1String("bin")) {
-                dir.cd("lib");
-                dir.cd("plugins");
-                dir.cd("inputmethods");
-                found = true;
-                break;
-            }
-        }
-    }
-    QVERIFY(found);
-    QPluginLoader loader(dir.absoluteFilePath("libim-qimsys-qt.so"));
-    QObject *object = loader.instance();
-    QInputContext *ic = 0;
-    if (object) {
-        QInputContextPlugin* plugin = qobject_cast<QInputContextPlugin*>(object);
-        if (plugin && plugin->keys().contains("qimsys")) {
-            ic = plugin->create("qimsys");
-        } else {
-            delete object;
-        }
-    }
-    QVERIFY(ic);
-    qApp->setInputContext(ic);
-
     keyManager.init();
     if (keyManager.hasError()) {
         QFAIL("Run qimsys by yourself first.");
@@ -129,10 +102,9 @@ void JapaneseStandardTest::initTestCase() {
 
     widget = new QPlainTextEdit;
     widget->show();
-#ifdef Q_WS_X11
-    qt_x11_wait_for_window_manager(widget);
-#endif
+    QTest::qWaitForWindowExposed(widget);
     widget->setFocus();
+    QTest::qWait(200);
 }
 
 void JapaneseStandardTest::init()
@@ -316,9 +288,9 @@ void JapaneseStandardTest::functionality()
                     if (item.to.at(i) != data) {
                         inputMethodManager.execute(QLatin1String("Select next candidate"));
                         QimsysConversionItemList candidateItems = candidateManager.items();
-                        for (int i = 0; i < candidateItems.length(); i++) {
-                            if (candidateItems.at(i).to == data) {
-                                candidateManager.setCurrentIndex(i);
+                        for (int j = 0; j < candidateItems.length(); j++) {
+                            if (candidateItems.at(j).to == data) {
+                                candidateManager.setCurrentIndex(j);
                                 QTest::qWait(100);
                                 break;
                             }
@@ -338,6 +310,32 @@ void JapaneseStandardTest::functionality()
     }
 }
 
-QTEST_MAIN(JapaneseStandardTest)
+int main(int argc, char *argv[])
+{
+    bool found = false;
+    QDir dir(QCoreApplication::applicationDirPath());
+    while (!found && dir.cdUp()) {
+        foreach(const QString &subdir, dir.entryList(QDir::Dirs)) {
+            if (subdir == QLatin1String("bin")) {
+                dir.cd("lib");
+                dir.cd("plugins");
+                found = true;
+                break;
+            }
+        }
+    }
+    if (!found) {
+        qFatal("!");
+    }
+
+    QByteArray qtPluginPath = qgetenv("QT_PLUGIN_PATH");
+    qtPluginPath.prepend(QStringLiteral("%1:").arg(dir.absolutePath()).toUtf8());
+    qputenv("QT_PLUGIN_PATH", qtPluginPath);
+    qputenv("QT_IM_MODULE", QByteArrayLiteral("qimsys"));
+
+    QApplication app(argc, argv);
+    JapaneseStandardTest tc;
+    return QTest::qExec(&tc, argc, argv);
+}
 
 #include "main.moc"

tests/system/inputmethods/japanese/standard/standard.pro
@@ -1,6 +1,8 @@
 TARGET = tst_japanese_standard
 include(../../../../../qimsystest.pri)
 
+QT += gui-private widgets
+
 SOURCES += main.cpp
 RESOURCES += main.qrc
 

commit 4dfa0ba72e94c92aebdb5b6ef72bebc19b9fe7f9

Fix a bug to input hennna
a regression introduced in 7ef895545f5c7bc5b45d40756019ffdfaff8b832

Change-Id: I4f9eb4b4ba46e8912818e2ee2d342ea2d562ddfa

src/plugins/interpreters/japanese/romaji/interpreter.cpp
@@ -237,10 +237,14 @@ void Interpreter::Private::itemChanged(const QimsysPreeditItem &item)
             }
             break;
         } else {
-            if (to.at(i) == from.at(i)) {
+            if (to.at(i) == from.at(i) && from.at(i) == rawString.at(i)) {
                 t += to.at(i);
                 f += from.at(i);
                 rS += rawString.at(i);
+            } else {
+                t.clear();
+                f.clear();
+                rS.clear();
             }
         }
     }